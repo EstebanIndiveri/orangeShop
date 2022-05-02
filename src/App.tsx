6e7319e46742a62d11e3671f137dd3e36686025d
Esteban Indiveri <esteban.indiveri@gmail.com>
2022-05-02 02:03:37 -0300

    kick off orange webstore

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ width: "50%" }}>
+          <h1>hola este es el primer titulo</h1>
+        </div>
+        <div style={{ width: "50%" }}>
+          <h1 className="app_hola">hola</h1>
+        </div>
+      </div>
+      <div
+        style={{
+          marginTop: "50px",
+          backgroundColor: "red",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <p style={{ fontSize: "40px" }}>Esta es la segunda sección xdxdd </p>
+      </div>
     </div>
   );
 }
